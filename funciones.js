65c7765e12c0e4d3c3ddece686d328cdc9a1a14f
Leyla Vasquez <Leylagisela@gmail.com>
2023-07-18 07:34:33 -0600

    Se agrego la rama project-2 utilizando objetos en la lista de contactos

--- funciones.js
+++ funciones.js
@@ -1,25 +1,48 @@
 
 
-
-let contactos=['Eldy Vasquez','Eddin Reyes','Jose Vasquez'];
-const arrayContactosActualizados=nuevoContacto(contactos,'Elsy Reyes');
-
-function nuevoContacto(listaContactos,nombreContacto)
+let contacto=[
 {
-    console.log("");
-    console.log("**Añade un contacto**");
-    listaContactos.push(nombreContacto);
-   imprimir(listaContactos, false);
-   return listaContactos;
-
+    id:1,
+    nombres:'Maria Paula',
+    apellidos:'Perez Sanchez',
+    telefono:95987888,
+    ubicaciones:{
+        ciudad:'Comayagua',
+        direccion:'11 calle 4 ave NE'}
 }
+]
 
-function eliminarContacto(arrayContactos)
+imprimir(contacto, true);
+
+nuevoContacto(2,'Juana Griselda', 'Gomez Discua', 89554577, 'Comayagua', 'Frente al Hospital general');
+eliminarContacto(contacto);
+function nuevoContacto(id, nombres, apellidos, telefono,ciudad,direccion)
+{
+    let nuevo={//funsion
+        id:id,
+        nombres:nombres,
+        apellidos:apellidos,
+        telefono:telefono,
+        ubicaciones:{ciudad:ciudad,
+                    direccion:direccion}
+       }
+      contacto.push(nuevo);
+      console.log("**********Agrega un contacto**********"); 
+   imprimir(contacto, "true");
+   }
+
+  
+
+
+
+
+
+function eliminarContacto(contacto)
 {
     console.log("");
-    console.log("**Elimina un contacto**");
-    arrayContactos.shift();
-    imprimir(arrayContactos, false);
+    console.log("**********Elimina un contacto**********");
+    contacto.shift();
+    imprimir(contacto, false);
 
 }
 
@@ -27,19 +50,23 @@ function imprimir(misContactos, mostrarEncabezado)
 {
     if (mostrarEncabezado===true)
     {
-    console.log('**Lista de contactos**');
+    console.log('************MI LISTA DE CONTACTOS**********');
     console.log("");  
     }
 
     for(let i=0;i<=misContactos.length-1;i++)
     {
-        console.log(misContactos[i]);
-    
+        console.log("Contacto No." + (i+1));
+        console.log("1. " + "id= " + contacto[i].id);
+        console.log("2. " + "Nombres= " + contacto[i].nombres);
+        console.log("3. " + "Apellidos= " + contacto[i].apellidos);
+        console.log("4. " + "Teléfono= " + contacto[i].telefono);
+        console.log("5. " + "Ubicaciones ");
+        console.log("      a." + contacto[i].ubicaciones.ciudad);
+        console.log("      b." + contacto[i].ubicaciones.direccion);
+        
+        console.log(' ');
+       // console.log(contacto[i]);
+
     }  
 }
-
-
-
-
-
-
